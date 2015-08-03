412ecab7f1e66c5f0fa46fbca79447c8e7a7b84c
Olivia Davis <oliviamidoridavis@gmail.com>
2015-08-03 12:54:39 -0400

    Plz work

--- Quarterly/main.py
+++ Quarterly/main.py
@@ -15,10 +15,23 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+from google.appengine.ext import ndb
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template_values = {
+            'test' : 'working'
+        }
+        template = JINJA_ENVIRONMENT.get_template('html/login.html')
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
