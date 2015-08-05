3436b73ad9bed2c2c9e61e69e1600069e9a06a17
Demo User <demouser@MacBook-Air-3.local>
2015-08-05 12:00:27 -0400

    why

--- capquarterly/main.py
+++ capquarterly/main.py
@@ -71,15 +71,10 @@ class GoalHandler(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('html/ind_goal.html')
         # Put in Giacomo's page line 41 from test to whatever he's named it
         self.response.write(template.render(template_values))
-<<<<<<< HEAD
-    #     # put underneath the creation of goal records in datastore
+    # put underneath the creation of goal records in datastore
     # def post(self):
-=======
-
-    def post(self):
-        # put here the creation of goal records in datastore
-        pass
->>>>>>> b13a7b81a622fba2211b3eca3eabebcaf9390869
+    #     # put here the creation of goal records in datastore
+    #     pass
 class Goal_pageHandler(webapp2.RequestHandler):
     def get(self):
         template_values = {
